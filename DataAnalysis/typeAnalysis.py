28a6c7379cf4a7f8754d58a11d67f16fdc1a3c9c
MashiroCl <441016116@qq.com>
2021-12-15 20:31:06 +0900

    write disappear to csv

--- DataAnalysis/typeAnalysis.py
+++ DataAnalysis/typeAnalysis.py
@@ -1,3 +1,9 @@
+import sys
+import pandas as pd
+sys.path.append('../')
+from DataAnalysis.setAnalysis import getSet
+from MyRepository import create_folder
+
 def buildRODict(roSet):
     resDict = dict()
     for each in roSet:
@@ -17,12 +23,11 @@ def setDivision(seta, setb):
         resDict[a] = seta.get(a) / setb.get(a, 1)
     return resDict
 
-
 def ROTypeAnalysis():
     # args = sys.argv
     # repoName = args[1]
     repoName = "refactoring-toy-example"
-    repoName = "retrolambda"
+    # repoName = "retrolambda"
 
     # experimentPath = "/home/chenlei/RA/setversion/experimentResult/"
     experimentPath = "/Users/leichen/ResearchAssistant/InteractiveRebase/experimentResult/"
@@ -33,11 +38,11 @@ def ROTypeAnalysis():
     set4by4 = getSet(experimentPath + repoName + "/4/")
 
     squashedSets = set2by2.union(set3by3.union(set4by4))
-    print("squashedSets", buildRODict(squashedSets))
+    # print("squashedSets", buildRODict(squashedSets))
     disappearRODict = buildRODict(set1by1 - squashedSets)
     set1by1RODict = buildRODict(set1by1)
-    print("disappearRODict", disappearRODict)
-    print("set1by1RODict", set1by1RODict)
+    # print("disappearRODict", disappearRODict)
+    # print("set1by1RODict", set1by1RODict)
 
     'Calculate the number of disappeared RO in squashed set divide the number of Ro of same type in no squash set'
     "Example result: {'Pull Up Method': 1.0, 'Move Class': 0.5} means "
@@ -59,8 +64,21 @@ def ROTypeAnalysis():
 
     all = set1by1.union(set2by2.union(set3by3.union(set4by4)))
     missingRatio = setDivision(buildRODict((all-set1by1)),buildRODict(all))
-    print("Missing Ratio: {}".format(disappearDictRatio))
+    print("Missing Ratio: {}".format(missingRatio))
 
+    belongedCommits = list()
+    types = list()
+    descriptions = list()
+    for each in set1by1 - squashedSets:
+        belongedCommits.append(each.belongedCommit)
+        types.append(each.type)
+        descriptions.append(each.description)
+        print("commitID:{}, type: {}, description: {}".format(each.belongedCommit,each.type,each.description))
+        dataFrame = pd.DataFrame({"commitID":belongedCommits,"type":types,"description":descriptions})
 
+    import os
+    csv_path = os.path.join("/Users/leichen/ResearchAssistant/csv/",repoName)
+    create_folder(csv_path)
+    dataFrame.to_csv(os.path.join(csv_path,"disappear.csv"),index=False,sep=",")
 if __name__ == "__main__":
     ROTypeAnalysis()
