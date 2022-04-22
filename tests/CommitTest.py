da13ed36f25cb18b9b951869c89af876f84dcced
MashiroCl <441016116@qq.com>
2022-04-22 16:01:03 +0900

    Update: change example repository from jfinal to refactoring-toy-example

--- tests/CommitTest.py
+++ tests/CommitTest.py
@@ -3,11 +3,11 @@ import json
 from commitProcess.Commit import Commit
 
 class CommitTest(unittest.TestCase):
-    def test_init_jfinal_gitLog_as_Commit_length_should_be_474(self):
-        path = "/Users/leichen/ResearchAssistant/InteractiveRebase/data/jfinal/gitLog.json"
+    def test_init_refactoring_toy_example_gitLog_as_Commit_length_should_be_474(self):
+        path = "gitLog.json"
         with open(path) as f:
             data = json.loads("[" + f.read() + "]")
         commits = []
         for each in data:
             commits.append(Commit(each))
-        self.assertEqual(len(commits), 474)
+        self.assertEqual(len(commits), 65)
