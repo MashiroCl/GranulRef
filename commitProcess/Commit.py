492fa3a79fd94a373201c48f1e1182daf2efb097
MashiroCl <441016116@qq.com>
2022-04-22 15:30:41 +0900

    Update: Comment out method getConnections Comment out method getAdded() Comment out method setAdded() add comments

--- commitProcess/Commit.py
+++ commitProcess/Commit.py
@@ -1,47 +1,59 @@
-import json
+"""
+git commit class
+"""
+
 
 class Commit:
-    def __init__(self,info:str):
-        self.commitID=info["commit"]
-        self.parentID=info["parent"].split(" ")
-        self.suject=None
-        self.commit_notes=info["commit_notes"]
-        self.commiter=info["commiter"]
-        self.isMerge=True if len(self.parentID)>1 else False
-        self.parent=[]
-        self.child=[]
-        self.added=False
-        self.head=False
+    """
+    turn json formatgir commit info into Commit Class
+    """
+    def __init__(self, info: str):
+        self.commitID = info["commit"]
+        self.parentID = info["parent"].split(" ")
+        self.suject = None
+        self.commit_notes = info["commit_notes"]
+        self.commiter = info["commiter"]
+        self.isMerge = True if len(self.parentID) > 1 else False
+        self.parent = []
+        self.child = []
+        # self.added=False
+        self.head = False
 
-    def setParent(self,pCommit)->None:
+    def setParent(self, pCommit) -> None:
+        """
+        set pCommit as the parent commit of the current one
+        :param pCommit:
+        :return:
+        """
         self.parent.append(pCommit)
 
-    def setChild(self,cCommit)->None:
+    def setChild(self, cCommit) -> None:
+        """
+        set cCommit as the child commit of thecurrent one
+        :param cCommit:
+        :return:
+        """
         self.child.append(cCommit)
 
-    def getConnections(self):
-        return self.connectedTo
+    # def getConnections(self):
+    #     """
+    #
+    #     :return:
+    #     """
+    #     return self.connectedTo
 
-    def getAdded(self):
-        return self.added
-
-    def setAdded(self):
-        self.added=True
+    # def getAdded(self):
+    #     return self.added
+    #
+    # def setAdded(self):
+    #     self.added=True
 
     def getHead(self):
         return self.head
-    def setHead(self):
-        self.head=True
 
-if __name__ =="__main__":
-    path="/Users/leichen/ResearchAssistant/InteractiveRebase/data/jfinal/test.json"
-    with open(path) as f:
-        data=json.loads("["+f.read()+"]")
-    data=data[0]
-    commits=[]
-    num=0
-    for each in data:
-        num=num+1
-        commits.append(Commit(each))
-    print(len(commits))
-    print(num)
+    def setHead(self):
+        """
+        set commit as the head(1st) commit of a sequence of commit
+        :return:
+        """
+        self.head = True
