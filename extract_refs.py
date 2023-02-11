799572afaa0dc285a6704cc3f6bd5a390a3da121
Lei Chen <mashirocl@gmail.com>
2023-02-11 19:37:34 +0900

    ref: move methods to Commit

--- extract_refs.py
+++ extract_refs.py
@@ -1,10 +1,16 @@
+import pathlib
 import os
+import glob
 from ROExtract.extractRO import extractRO
 from logger import logger_config
 from utils import get_config
+from repository import Repository
+from refactoring_operation.commit import Commit
+from line_trace import load_commit_pairs, get_parent_commit
+from typing import Union
 
 
-def make_directories(output: str, repo_name, cluster_num):
+def make_directories(output: str, repo_name: str, cluster_num: int):
     """
     -repo_name/
     ----cluster_num/
@@ -52,3 +58,72 @@ def run(repo_path, output, cluster_num, platform):
     logger.info("finish squash " + str(cluster_num) + "by" + str(cluster_num))
 
     os.system("rm -rf " + squashed_git_p)
+
+
+def attach_source_locations(repo_path: str, squash_log_d: str, squash_num_start=2, squash_num_end=5) -> None:
+    '''
+    trace source locations for refs in commits in squash_log_d and dump the refs into json files
+    :param repo_path: repository where commits belong
+    :param squash_log_d: squash result directory
+    :param squash_num_start: start squash num
+    :param squash_num_end: end squash num
+    :return: None
+    '''
+
+    def commits_trace_1(commits: list[Commit]) -> None:
+        """
+        trace for non-squash commits, trace from their parent commits
+        :param commits:
+        :return:
+        """
+        for c in commits:
+            c_parent = get_parent_commit(c.sha1, repo)
+            if c_parent:
+                c.trace_refs_source_locations(
+                    parent_commit_sha1=c_parent,
+                    repo=repo
+                )
+
+    def commits_trace_n(commits: list[Commit], pairs: dict[str, list[str]]):
+        """
+        trace for squashed commits (coarse-granied commits). For each coarse-grained commit, in the fine-grained commits
+        which squashed into it, find the most early fine-grained one in terms of chronological order. Trace is conduct
+        on the fine-grained commit's parent commit
+        :param commits: coarse-grained commits list
+        :param paris: dict where key is the coarse-grained commits and value is the fine-grained commits which squashed into the coarse-grained one
+        :return:
+        """
+        for coarse_grained_commit in commits:
+            coarse_c_parent = get_parent_commit(pairs[coarse_grained_commit.sha1][-1], repo)
+            if coarse_c_parent:
+                coarse_grained_commit.trace_refs_source_locations(
+                    parent_commit_sha1=coarse_c_parent,
+                    repo=repo
+                )
+
+    def commits_dump(commits: list[Commit], directory: str) -> None:
+        for c in commits:
+            c.dump_refs(directory)
+
+    squash_log_d = pathlib.Path(squash_log_d)
+    repo = Repository(repo_path)
+    for squash_num in range(squash_num_start, squash_num_end + 1):
+        squash_log_p = squash_log_d.joinpath(str(squash_num)).joinpath(f"log{squash_num}.txt")
+        refs_dir = squash_log_p.parent.joinpath("refs")
+        output_directory = squash_log_d.joinpath(f"oo{squash_num}").__str__()
+
+        if squash_num == 1:
+            cs = [Commit(file) for file in get_json_files_under_directory(refs_dir)]
+            commits_trace_1(cs)
+            commits_dump(cs, output_directory)
+
+        else:
+            pairs = load_commit_pairs(squash_log_p.__str__())
+            coarse_grained_commits = [Commit(refs_dir.joinpath(c + ".json")) for c in pairs.keys()]
+            commits_trace_n(coarse_grained_commits, pairs)
+            commits_dump(coarse_grained_commits, output_directory)
+
+
+def get_json_files_under_directory(directory: Union[str, pathlib.Path]) -> list[str]:
+    directory = directory if isinstance(directory, pathlib.Path) else pathlib.Path(directory)
+    return glob.glob(directory.joinpath("*.json").__str__())
--- refactoring_operation/commit.py
+++ refactoring_operation/commit.py
@@ -1,12 +1,55 @@
 import json
-from refactoring_operation.refactoring import RefRM
+from refactoring_operation.refactoring import Refactoring, RefRM
+import pathlib
+import subprocess
 
-class Commit():
-    def __init__(self, p):
-        pass
+
+class Commit:
+    """
+    Commit load from json file, json file can be output of refactoring miners (RefactoringMiner, RefDiff)
+    """
+
+    def __init__(self, file_p):
+        with open(file_p) as f:
+            data = json.load(f)
+        self.sha1 = pathlib.Path(file_p).name.split(".")[0]
+        self.commit_raw = data.get("commits", None)
+        self.refs = []
+        if self.commit_raw:
+            self.refs = self._extrac_refs()
 
     def get_refs(self):
-        pass
+        return self.refs
+
+    def _extrac_refs(self):
+        refs = self.commit_raw[0].get("refactorings", None)
+        if refs:
+            refs = [Refactoring(r) for r in refs]
+        return refs
+
+    def dump_refs(self, directory: str):
+        """
+        dump
+        :param directory:
+        :return:
+        """
+        directory = pathlib.Path(directory)
+        if not directory.exists():
+            directory.mkdir()
+        file = directory.joinpath(self.sha1 + ".json")
+        if len(self.refs):  # exclude commits with no refs
+            with open(file, "w") as f:
+                json.dump([ref.get_dict_format() for ref in self.refs], f)
+
+    def trace_refs_source_locations(self, parent_commit_sha1: str, repo: 'Repository') -> 'Commit':
+        """
+        trace refs source locations
+        :return:
+        """
+        for ref in self.refs:
+            ref.trace_source_location(parent_commit_sha1, repo)
+        return self
+
 
 class RMCommit(Commit):
     def __init__(self, p):
@@ -14,6 +57,7 @@ class RMCommit(Commit):
         initialize the commit with a RefactoringMiner json output
         :param p: path for the RefactoringMiner json output
         '''
+        super().__init__(p)
         self.refs = None
         with open(p) as f:
             data = json.load(f)
