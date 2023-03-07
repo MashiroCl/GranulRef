5e7299eb5e06e0daca61ca8a1f5d7c6b8fc9a00d
Lei Chen <mashirocl@gmail.com>
2023-03-07 16:23:39 +0900

    feat: extract refs by squashlog

--- match.py
+++ match.py
@@ -23,16 +23,54 @@ def get_commit_refdict(squash_log_p: str, commits: Union[str, list[str]]) -> lis
     if isinstance(commits, list):  # fine grained commits
         fine_grained_path = refs_d.joinpath("o1")
         for commit in commits:
-            refs.append(load_dictref(fine_grained_path.joinpath(commit+".json")))
-    else:       # coarse grained commits
+            refs.append(load_dictref(fine_grained_path.joinpath(commit + ".json")))
+    else:  # coarse grained commits
         coarse_grained_path = refs_d.joinpath(f"o{squash_num}")
-        refs.append(load_dictref(coarse_grained_path.joinpath(commits+".json")))
+        refs.append(load_dictref(coarse_grained_path.joinpath(commits + ".json")))
     return refs
 
 
 def extract_coarse_grained_refs(coarse_grained_refs: list[list[Refactoring]],
                                 fine_grained_refs: list[list[Refactoring]]) -> list[Refactoring]:
     return list(set(coarse_grained_refs[0]) - set([ref for each in fine_grained_refs for ref in each]))
+
+
+def extract_coarse_grained_refs_by_squashlog(squash_log_p: str) -> list[Refactoring]:
+    d = load_commit_pairs(squash_log_p)
+    res = list()
+    for coarse_grained_commit in d.keys():
+        fine_grained_commits = d[coarse_grained_commit]
+        fine_grained_refs = get_commit_refdict(squash_log_p, list(fine_grained_commits))
+        coarse_grained_refs = get_commit_refdict(squash_log_p, coarse_grained_commit)
+        cgr = extract_coarse_grained_refs(coarse_grained_refs, fine_grained_refs)
+        if len(cgr) > 0:
+            res += cgr
+    return res
+
+
+def extract_fine_grained_refs_by_squashlog(squash_log_p: str) -> list[Refactoring]:
+    d = load_commit_pairs(squash_log_p)
+    res = list()
+    for coarse_grained_commit in d.keys():
+        fine_grained_commits = d[coarse_grained_commit]
+        fine_grained_refs = get_commit_refdict(squash_log_p, list(fine_grained_commits))
+        coarse_grained_refs = get_commit_refdict(squash_log_p, coarse_grained_commit)
+        fgr = extract_fine_grained_refs(coarse_grained_refs, fine_grained_refs)
+        if len(fgr) > 0:
+            res += fgr
+    return res
+
+
+def extract_regular_refs_by_squashlog(squash_log_p: str) -> list[Refactoring]:
+    squash_log_p = pathlib.Path(squash_log_p)
+    refs_d = pathlib.Path(squash_log_p).parent.parent
+    if not squash_log_p.name == "log1.txt":
+        raise RuntimeError(f"squash log is {squash_log_p}, use log1.txt as the squash log")
+    refs = []
+    fine_grained_path = refs_d.joinpath("o1")
+    for path in fine_grained_path.iterdir():
+        refs+=load_dictref(path)
+    return refs
 
 
 def get_coarse_grained_refs(squash_log_p: str) -> list[Refactoring]:
@@ -45,3 +83,8 @@ def get_coarse_grained_refs(squash_log_p: str) -> list[Refactoring]:
         CGRs += extract_coarse_grained_refs(coarse_grained_refs, fine_grained_refs)
 
     return CGRs
+
+
+def extract_fine_grained_refs(coarse_grained_refs: list[list[Refactoring]],
+                              fine_grained_refs: list[list[Refactoring]]) -> list[Refactoring]:
+    return list(set([ref for each in fine_grained_refs for ref in each]) - set(coarse_grained_refs[0]))
