97232c48b01dabd256591010e7b95734a807c3a5
Lei Chen <mashirocl@gmail.com>
2023-03-07 15:04:34 +0900

    feat: able to trace (attach original line) through -m option

--- command.py
+++ command.py
@@ -1,5 +1,5 @@
 import argparse
-from extract_refs import run
+from extract_refs import run, attach_source_locations
 
 
 def commands():
@@ -8,9 +8,18 @@ def commands():
     parser.add_argument("-c", help="number of commits in a cluster")
     parser.add_argument("-o", help="output path")
     parser.add_argument("-p", help="platform")
+
+    parser.add_argument("-m", help ="mode selection, 'extract' for extracting refactorings from commits in a repository"
+                                    "trace for tracing source locations for refactorings")
+    parser.add_argument("-d", help = "squash result directory")
+    parser.add_argument("--start", help="start squash num")
+    parser.add_argument("--end", help="end squash num")
     return parser
 
 
 if __name__ == "__main__":
     args = commands().parse_args()
-    run(args.r, args.o, int(args.c), args.p)
+    if args.m == "trace":
+        attach_source_locations(args.r, args.d, args.start, args.end)
+    else:
+        run(args.r, args.o, int(args.c), args.p)
