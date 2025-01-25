67759787af67db8fbf319e818105bb3242d94e1d
Lei Chen <mashirocl@gmail.com>
2025-01-25 15:49:04 +0900

    fix: not attach description if the ref has no leftSideLocations

--- attach_descriptions.py
+++ attach_descriptions.py
@@ -7,17 +7,20 @@ def overwrite_add_description(f_d: pathlib.Path, f_no_d: pathlib.Path):
     def build_key(ref):
         key = {"type": ref["type"]}
 
-        leftSideLocation = ref["leftSideLocations"][0]
-        key["leftSideLocations"] = {}
-        key["leftSideLocations"]["startLine"] = leftSideLocation["startLine"]
-        key["leftSideLocations"]["endLine"] = leftSideLocation["endLine"]
-        key["leftSideLocations"]["filePath"] = leftSideLocation["filePath"]
-        key["leftSideLocations"]["codeElement"] = leftSideLocation["codeElement"]
+        try:
+            leftSideLocation = ref["leftSideLocations"][0]
+            key["leftSideLocations"] = {}
+            key["leftSideLocations"]["startLine"] = leftSideLocation["startLine"]
+            key["leftSideLocations"]["endLine"] = leftSideLocation["endLine"]
+            key["leftSideLocations"]["filePath"] = leftSideLocation["filePath"]
+            key["leftSideLocations"]["codeElement"] = leftSideLocation["codeElement"]
 
-        rightSideLocation = ref["rightSideLocations"][0]
-        key["rightSideLocations"] = {}
-        key["rightSideLocations"]["filePath"] = rightSideLocation["filePath"]
-        key["rightSideLocations"]["codeElement"] = rightSideLocation["codeElement"]
+            rightSideLocation = ref["rightSideLocations"][0]
+            key["rightSideLocations"] = {}
+            key["rightSideLocations"]["filePath"] = rightSideLocation["filePath"]
+            key["rightSideLocations"]["codeElement"] = rightSideLocation["codeElement"]
+        except IndexError:
+            key = ""
 
         return str(key)
 
@@ -26,15 +29,18 @@ def overwrite_add_description(f_d: pathlib.Path, f_no_d: pathlib.Path):
     ref_descriptions = {}
     for ref in ref_json_des["commits"][0]["refactorings"]:
         key = build_key(ref)
-        ref_descriptions[str(key)] = ref["description"]
+        if len(key):
+            ref_descriptions[str(key)] = ref["description"]
 
     with open(f_no_d, "r") as f:
         ref_json_no_des = json.load(f)
         for ref in ref_json_no_des["refactorings"]:
             key = build_key(ref)
-            ref["description"] = ref_descriptions.get(key, "")
+            if len(key):
+                ref["description"] = ref_descriptions.get(key, "")
     with open(f_no_d, "w") as f:
         json.dump(ref_json_no_des, f)
+
 
 def attach_descriptions(repo_path):
     for i in range(1, 2):
@@ -53,7 +59,9 @@ if __name__ == "__main__":
     # repo_path = "/Users/leichen/Code/pythonProject/pythonProject/pythonProject/SCRMDetection/experiment/output/result/mbassador_cr"
     # attach_descriptions(repo_path)
 
-    parser = argparse.ArgumentParser(description="Attach refactoring descriptions to the traced refactoring files")
+    parser = argparse.ArgumentParser(
+        description="Attach refactoring descriptions to the traced refactoring files"
+    )
     parser.add_argument("-r", help="repository path")
     args = parser.parse_args()
     attach_descriptions(args.r)
